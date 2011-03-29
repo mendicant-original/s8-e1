3d8ee1502d32841fd7d31446d7d13c6d3e2b0426
eregon <eregontp@gmail.com>
2011-03-29 06:49:56 +0200

    add a few examples to generate tests

--- spec/generate_spec.rb
+++ spec/generate_spec.rb
@@ -4,14 +4,16 @@ describe 'generate' do
   Examples = {
     :__gmpz_init          => [[:pointer], :void],
     :__gmpz_init_set_si   => [[:pointer, :long], :void],
+    :__gmpz_add           => [[:pointer, :pointer, :pointer], :void],
     :__gmpz_mul_si        => [[:pointer, :pointer, :long], :void],
     :__gmp_printf         => [[:string, :varargs], :int],
     :__gmpz_get_str       => [[:string, :int, :pointer], :string],
     :__gmpz_fits_slong_p  => [[:pointer], :int],
+    :__gmpz_get_si        => [[:pointer], :long],
   }
 
   Examples.each_pair { |name, types|
-    it "function #{name} types" do
+    it name do
       Lib::Functions[name].should == types
     end
   }
