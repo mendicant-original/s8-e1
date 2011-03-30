bbf199b4ee9037d7b7f7e72a22b32039dbe6f2e9
eregon <eregontp@gmail.com>
2011-03-30 04:43:24 +0200

    test: minimal change to be able to run the test against gmp-ffi

--- tests_from_gmp_gem/test_helper.rb
+++ tests_from_gmp_gem/test_helper.rb
@@ -2,8 +2,4 @@ require 'test/unit'
 require './test_unit/assertions'  # Monkey patch
 require 'rbconfig'
 
-ENV['PATH'] = [File.expand_path(
-  File.join(File.dirname(__FILE__), "..", "ext")
-), ENV['PATH']].compact.join(';') if RbConfig::CONFIG['host_os'] =~ /(mswin|mingw|mingw32)/i
-
-require File.dirname(__FILE__) + '/../ext/gmp'
+require_relative '../lib/gmp-ffi'
