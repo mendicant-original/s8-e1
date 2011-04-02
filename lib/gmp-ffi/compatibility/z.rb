62702c3492984745cf09209b43a5dccbc456e71a
eregon <eregontp@gmail.com>
2011-04-02 17:17:12 +0200

    split compatibility by class

---
+++ lib/gmp-ffi/compatibility/z.rb
@@ -0,0 +1,33 @@
+require_relative '../z'
+
+module GMP
+  class Z
+    def jacobi(b)
+      Z.jacobi(self, b)
+    end
+    def legendre(p)
+      raise RangeError if p.composite?
+      Z.jacobi(self, p)
+    end
+
+    alias :nextprime :next_prime
+    alias :com :~
+
+    alias :old_addmul! :addmul!
+    def addmul!(a, b)
+      raise RangeError if Fixnum === b and b < 0
+      old_addmul!(a, b)
+    end
+
+    alias :old_submul! :submul!
+    def submul!(a, b)
+      raise RangeError if Fixnum === b and b < 0
+      old_submul!(a, b)
+    end
+
+    class << self
+      alias :fac :factorial
+      alias :fib :fibonacci
+    end
+  end
+end
