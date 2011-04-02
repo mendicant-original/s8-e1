2488a653339a96a9567de12985e77983abb9e70b
eregon <eregontp@gmail.com>
2011-04-02 04:20:52 +0200

    use better errors for extconf failures

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -1,7 +1,7 @@
 require 'mkmf'
 
 dir_config('gmp')
-fail "can't find gmp.h, try --with-gmp-include=<path>" unless have_header('gmp.h')
-fail "can't find -lgmp, try --with-gmp-lib=<path>" unless have_library('gmp', '__gmpz_init')
+fail "can't find the gmp.h header, try --with-gmp-include=<path>" unless have_header('gmp.h')
+fail "can't find the gmp library, try --with-gmp-lib=<path>" unless have_library('gmp', '__gmpz_init')
 
 create_makefile 'gmp_ffi'
