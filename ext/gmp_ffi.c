ecbc90d8c349149a9cad9a5800914b5d8f3cd0c4
eregon <eregontp@gmail.com>
2011-04-02 03:48:28 +0200

    fix mpz_t to Z when mpz_t < 0

--- ext/gmp_ffi.c
+++ ext/gmp_ffi.c
@@ -15,7 +15,7 @@ VALUE mpz2num(mpz_t z) {
     return LONG2NUM(mpz_get_si(z));
   } else {
     long size = (mpz_sizeinbase(z, 2) + 8*SIZEOF_BDIGITS-1) / (8*SIZEOF_BDIGITS);
-    VALUE big = rb_big_new(size, 1);
+    VALUE big = rb_big_new(size, mpz_sgn(z) >= 0);
     BDIGIT *digits = RBIGNUM_DIGITS(big);
     mpz_export(digits, NULL, -1, SIZEOF_BDIGITS, 0, 0, z);
     return big;
--- spec/shared_examples.rb
+++ spec/shared_examples.rb
@@ -1,6 +1,8 @@
+SharedExamplesNumbers = [-18446462598732906496, -2**62-1, -2**62, -2**30-1, -2**30, -7, 42, 2**30-1, 2**30, 2**62-1, 2**62, 18446462598732906496]
+
 shared_examples_for 'Z#to_i' do |meth|
   it "Z##{meth}" do
-    [-2**62-1, -2**62, -2**30-1, -2**30, -7, 42, 2**30-1, 2**30, 2**62-1, 2**62].each { |n|
+    SharedExamplesNumbers.each { |n|
       i = Z.new(n).send(meth)
       n.should == i
       i.class.should be n.class
@@ -10,7 +12,7 @@ end
 
 shared_examples_for 'Z#from_i' do |meth|
   it "Z##{meth}" do
-    [-2**62-1, -2**62, -2**30-1, -2**30, -7, 42, 2**30-1, 2**30, 2**62-1, 2**62].each { |n|
+    SharedExamplesNumbers.each { |n|
       z = Z.new
       z.send(meth, n)
       z.should == n
